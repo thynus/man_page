fb5e11e32d60f509fc62d5839f28b864222ff1c1
thynus <quabid@gmail.com>
2024-11-10 20:46:13 -0800

    Create man_page.py

---
+++ man_page.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#
+## author: thynus
+## source:
+#  https://github.com/fastfetch-cli/fastfetch/blob/dev/src/data/help.json
+#
+## requirements:
+#  sudo apt install help2man
+#
+## usage:
+#  ./man_page.py > fastfetch.1.in
+#
+#
+import subprocess
+
+subprocess.run(['help2man','fastfetch'])
